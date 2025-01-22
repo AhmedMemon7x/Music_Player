770380a83a8c59525b626808b75c141967be42ac
Ahmed Memon <127344463+AhmedMemon7x@users.noreply.github.com>
2025-01-22 16:58:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -29,37 +29,37 @@ let range =progressBar.value
 // alert(range)
 
 let currentSon=audio.src;
-if(currentSon=="http://127.0.0.1:5500/audio/KAUNTALHA.mp3" && range==100){
+if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/KAUNTALHA.mp3" && range==100){
         audio.src="./audio/SECRETS.mp3"
         artistName.innerHTML="SECRETS"
         track_img.src="./img/talha-anjum-open-letter.webp"
     } 
-   else if(currentSon=="http://127.0.0.1:5500/audio/SECRETS.mp3" && range==100){
+   else if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/SECRETS.mp3" && range==100){
         audio.src="./audio/AGENCY.mp3"
         artistName.innerHTML="AGENCY"
                 track_img.src="./img/maxresdefault.jpg"
             } 
-            else if(currentSon=="http://127.0.0.1:5500/audio/AGENCY.mp3" && range==100){
+            else if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/AGENCY.mp3" && range==100){
         audio.src="./audio/AFSANAY.mp3"
         artistName.innerHTML="AFSANAY"
         track_img.src="./img/asf.jpg"
     } 
-    else if(currentSon=="http://127.0.0.1:5500/audio/AFSANAY.mp3" && range==100){
+    else if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/AFSANAY.mp3" && range==100){
         audio.src="./audio/ME%20AND%20YOU.mp3"
         artistName.innerHTML="ME & YOU"
         track_img.src="./img/me.jpg"
     } 
-    else if(currentSon=="http://127.0.0.1:5500/audio/ME%20AND%20YOU.mp3" && range==100){
+    else if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/ME%20AND%20YOU.mp3" && range==100){
         audio.src="./audio/COME_THROUGH.mp3"
         artistName.innerHTML="COME THROUGH"
         track_img.src="./img/come.jpg"
     } 
-    else if(currentSon=="http://127.0.0.1:5500/audio/COME_THROUGH.mp3" && range==100){
+    else if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/COME_THROUGH.mp3" && range==100){
         audio.src="./audio/TWOTONE.mp3"
         artistName.innerHTML="TWO TONE"
         track_img.src="./img/twot.jpg"
     } 
-    else if(currentSon=="http://127.0.0.1:5500/audio/TWOTONE.mp3" && range==100){
+    else if(currentSon=="https://ahmedmemon7x.github.io/Music_Player/audio/TWOTONE.mp3" && range==100){
         audio.src="./audio/KAUNTALHA.mp3"
         artistName.innerHTML="KAUN TALHA"
         track_img.src="./img/kaunn.jpeg"
@@ -69,37 +69,37 @@ if(currentSon=="http://127.0.0.1:5500/audio/KAUNTALHA.mp3" && range==100){
 
 next_btn.addEventListener('click',()=>{
 let currentSong=audio.src;
-if(currentSong=="http://127.0.0.1:5500/audio/KAUNTALHA.mp3"){
+if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/KAUNTALHA.mp3"){
         audio.src="./audio/SECRETS.mp3"
         artistName.innerHTML="SECRETS"
         track_img.src="./img/talha-anjum-open-letter.webp"
     } 
-   else if(currentSong=="http://127.0.0.1:5500/audio/SECRETS.mp3"){
+   else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/SECRETS.mp3"){
         audio.src="./audio/AGENCY.mp3"
         artistName.innerHTML="AGENCY"
                 track_img.src="./img/maxresdefault.jpg"
             } 
-            else if(currentSong=="http://127.0.0.1:5500/audio/AGENCY.mp3"){
+            else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/AGENCY.mp3"){
         audio.src="./audio/AFSANAY.mp3"
         artistName.innerHTML="AFSANAY"
         track_img.src="./img/asf.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/AFSANAY.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/AFSANAY.mp3"){
         audio.src="./audio/ME%20AND%20YOU.mp3"
         artistName.innerHTML="ME & YOU"
         track_img.src="./img/me.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/ME%20AND%20YOU.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/ME%20AND%20YOU.mp3"){
         audio.src="./audio/COME_THROUGH.mp3"
         artistName.innerHTML="COME THROUGH"
         track_img.src="./img/come.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/COME_THROUGH.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/COME_THROUGH.mp3"){
         audio.src="./audio/TWOTONE.mp3"
         artistName.innerHTML="TWO TONE"
         track_img.src="./img/twot.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/TWOTONE.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/TWOTONE.mp3"){
         audio.src="./audio/KAUNTALHA.mp3"
         artistName.innerHTML="KAUN TALHA"
         track_img.src="./img/kaunn.jpeg"
@@ -108,37 +108,37 @@ if(currentSong=="http://127.0.0.1:5500/audio/KAUNTALHA.mp3"){
 
 prev_btn.addEventListener('click',()=>{
     let currentSong=audio.src;
-    if(currentSong=="http://127.0.0.1:5500/audio/KAUNTALHA.mp3"){
+    if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/KAUNTALHA.mp3"){
         audio.src="./audio/TWOTONE.mp3"
         artistName.innerHTML="TWO TONE"
         track_img.src="./img/twot.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/SECRETS.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/SECRETS.mp3"){
         audio.src="./audio/KAUNTALHA.mp3"
         artistName.innerHTML="KAUN TALHA"
         track_img.src="./img/kaunn.jpeg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/AGENCY.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/AGENCY.mp3"){
         audio.src="./audio/SECRETS.mp3"
         artistName.innerHTML="SECRETS"
         track_img.src="./img/talha-anjum-open-letter.webp"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/AFSANAY.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/AFSANAY.mp3"){
         audio.src="./audio/AGENCY.mp3"
         artistName.innerHTML="AGENCY"
         track_img.src="./img/maxresdefault.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/ME%20AND%20YOU.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/ME%20AND%20YOU.mp3"){
         audio.src="./audio/AFSANAY.mp3"
         artistName.innerHTML="AFSANAY"
         track_img.src="./img/asf.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/COME_THROUGH.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/COME_THROUGH.mp3"){
         audio.src="./audio/ME%20AND%20YOU.mp3"
         artistName.innerHTML="ME & YOU"
         track_img.src="./img/me.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/TWOTONE.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/TWOTONE.mp3"){
         audio.src="./audio/COME_THROUGH.mp3"
         artistName.innerHTML="COME THROUGH"
         track_img.src="./img/come.jpg"
@@ -158,22 +158,22 @@ artistName.innerHTML="MILLIONAIRE"
 next_btn.addEventListener('click',()=>{
     let currentSong=audio.src;
     // alert(currentSong)
-    if(currentSong=="http://127.0.0.1:5500/audio/MILLIONAIRE.mp3"){
+    if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/MILLIONAIRE.mp3"){
         audio.src="./audio/JATTMEHKMA.mp3"
         artistName.innerHTML="JATT MEHKMA"
         track_img.src="./img/jat.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/JATTMEHKMA.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/JATTMEHKMA.mp3"){
         audio.src="./audio/VIGDIYANHEERAN.mp3"
         artistName.innerHTML="VIGDIYAN HEERAN"
         track_img.src="./img/vig.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/VIGDIYANHEERAN.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/VIGDIYANHEERAN.mp3"){
         audio.src="./audio/BLUEEYES.mp3"
         artistName.innerHTML="BLUE EYES"
         track_img.src="./img/blu.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/BLUEEYES.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/BLUEEYES.mp3"){
         audio.src="./audio/MILLIONAIRE.mp3"
         artistName.innerHTML="MILLIONAIRE"
         track_img.src="./img/honeysingh.jpg"
@@ -182,22 +182,22 @@ next_btn.addEventListener('click',()=>{
 
 prev_btn.addEventListener('click',()=>{
     let currentSong=audio.src;
-    if(currentSong=="http://127.0.0.1:5500/audio/MILLIONAIRE.mp3"){
+    if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/MILLIONAIRE.mp3"){
         audio.src="./audio/BLUEEYES.mp3"
         artistName.innerHTML="BLUE EYES"
         track_img.src="./img/blu.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/JATTMEHKMA.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/JATTMEHKMA.mp3"){
         audio.src="./audio/MILLIONAIRE.mp3"
         artistName.innerHTML="MILLIONAIRE"
         track_img.src="./img/honeysingh.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/VIGDIYANHEERAN.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/VIGDIYANHEERAN.mp3"){
         audio.src="./audio/JATTMEHKMA.mp3"
         artistName.innerHTML="JATT MEHKMA"
         track_img.src="./img/jat.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/BLUEEYES.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/BLUEEYES.mp3"){
         audio.src="./audio/VIGDIYANHEERAN.mp3"
         artistName.innerHTML="VIGDIYAN HEERAN"
         track_img.src="./img/vig.jpg"
@@ -217,27 +217,27 @@ artistName.innerHTML="HOPE"
 next_btn.addEventListener('click',()=>{
     let currentSong=audio.src;
     // alert(currentSong)
-    if(currentSong=="http://127.0.0.1:5500/audio/HOPE.mp3"){
+    if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/HOPE.mp3"){
         audio.src="./audio/KILL ME.mp3"
         artistName.innerHTML="KILL ME"
     track_img.src="./img/xxxTentacion.jpg"
 } 
-else if(currentSong=="http://127.0.0.1:5500/audio/KILL%20ME.mp3"){
+else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/KILL%20ME.mp3"){
     audio.src="./audio/LOOK AT ME.mp3"
         artistName.innerHTML="LOOK AT ME"
         track_img.src="./img/lk.jpeg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/LOOK%20AT%20ME.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/LOOK%20AT%20ME.mp3"){
         audio.src="./audio/MOONLIGHT.mp3"
         artistName.innerHTML="MOONLIGHT"
         track_img.src="./img/mon.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/MOONLIGHT.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/MOONLIGHT.mp3"){
         audio.src="./audio/SAD.mp3"
         artistName.innerHTML="SAD"
         track_img.src="./img/sad.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/SAD.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/SAD.mp3"){
         audio.src="./audio/HOPE.mp3"
         artistName.innerHTML="HOPE"
         track_img.src="./img/hope.jpg"
@@ -246,27 +246,27 @@ else if(currentSong=="http://127.0.0.1:5500/audio/KILL%20ME.mp3"){
 
 prev_btn.addEventListener('click',()=>{
     let currentSong=audio.src;
-    if(currentSong=="http://127.0.0.1:5500/audio/HOPE.mp3"){
+    if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/HOPE.mp3"){
         audio.src="./audio/SAD.mp3"
         artistName.innerHTML="SAD"
         track_img.src="./img/sad.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/SAD.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/SAD.mp3"){
         audio.src="./audio/MOONLIGHT.mp3"
         artistName.innerHTML="MOONLIGHT"
         track_img.src="./img/mon.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/MOONLIGHT.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/MOONLIGHT.mp3"){
         audio.src="./audio/LOOK AT ME.mp3"
         artistName.innerHTML="LOOK AT ME"
         track_img.src="./img/lk.jpeg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/LOOK%20AT%20ME.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/LOOK%20AT%20ME.mp3"){
         audio.src="./audio/KILL ME.mp3"
         artistName.innerHTML="KILL ME"
         track_img.src="./img/xxxTentacion.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/KILL%20ME.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/KILL%20ME.mp3"){
         audio.src="./audio/HOPE.mp3"
         artistName.innerHTML="HOPE"
         track_img.src="./img/hope.jpg"
@@ -286,27 +286,27 @@ artistName.innerHTML="WAVY"
 next_btn.addEventListener('click',()=>{
     let currentSong=audio.src;
     // alert(currentSong)
-    if(currentSong=="http://127.0.0.1:5500/audio/WAVY.mp3"){
+    if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/WAVY.mp3"){
         audio.src="./audio/RENEGADE.mp3"
         artistName.innerHTML="RENEGADE"
         track_img.src="./img/aaryanshahrenegade.png"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/RENEGADE.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/RENEGADE.mp3"){
         audio.src="./audio/OSAHIB.mp3"
         artistName.innerHTML="O SAHIB"
         track_img.src="./img/O-Sahib.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/OSAHIB.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/OSAHIB.mp3"){
         audio.src="./audio/BIGDAWGS.mp3"
         artistName.innerHTML="BIG DAWGS"
         track_img.src="./img/BIGDAWGS.jpg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/BIGDAWGS.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/BIGDAWGS.mp3"){
         audio.src="./audio/ECSTACY.mp3"
         artistName.innerHTML="ECSTACY"
         track_img.src="./img/ECSTACY.jpeg"
     } 
-    else if(currentSong=="http://127.0.0.1:5500/audio/ECSTACY.mp3"){
+    else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/ECSTACY.mp3"){
         audio.src="./audio/WAVY.mp3"
         artistName.innerHTML="WAVY"
         track_img.src="./img/Wavykaranaujla.jpeg"
@@ -315,27 +315,27 @@ next_btn.addEventListener('click',()=>{
 
 prev_btn.addEventListener('click',()=>{
 let currentSong=audio.src;
-if(currentSong=="http://127.0.0.1:5500/audio/OSAHIB.mp3"){
+if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/OSAHIB.mp3"){
     audio.src="./audio/RENEGADE.mp3"
     artistName.innerHTML="RENEGADE"
     track_img.src="./img/aaryanshahrenegade.png"
 } 
-else if(currentSong=="http://127.0.0.1:5500/audio/BIGDAWGS.mp3"){
+else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/BIGDAWGS.mp3"){
     audio.src="./audio/OSAHIB.mp3"
     artistName.innerHTML="O SAHIB"
     track_img.src="./img/O-Sahib.jpg"
 } 
-else if(currentSong=="http://127.0.0.1:5500/audio/ECSTACY.mp3"){
+else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/ECSTACY.mp3"){
     audio.src="./audio/BIGDAWGS.mp3"
     artistName.innerHTML="BIG DAWGS"
     track_img.src="./img/BIGDAWGS.jpg"
 } 
-else if(currentSong=="http://127.0.0.1:5500/audio/WAVY.mp3"){
+else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/WAVY.mp3"){
     audio.src="./audio/ECSTACY.mp3"
     artistName.innerHTML="ECSTACY"
     track_img.src="./img/ECSTACY.jpeg"
 } 
-else if(currentSong=="http://127.0.0.1:5500/audio/RENEGADE.mp3"){
+else if(currentSong=="https://ahmedmemon7x.github.io/Music_Player/audio/RENEGADE.mp3"){
     audio.src="./audio/WAVY.mp3"
     artistName.innerHTML="WAVY"
     track_img.src="./img/Wavykaranaujla.jpeg"
